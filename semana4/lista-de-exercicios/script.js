9db4e9d2ea71a76f931439dfc420757a4e03a92f
Raiza Leal Franca <raizale@hotmail.com>
2020-12-11 11:36:53 -0300

    exercicios interpretação de codigo

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,35 @@
+//Exercícios de interpretação de código
+
+//1
+//Uma função que recebe um parâmentro, 
+//dentro da função é declarada uma variável que pega o valor do prompt e transforma em número
+//que retorna o parâmentro vezes o valor inserido no prompt.
+//fora da função é declarada uma variável que recebe a função com o parâmetro 100
+// no console imprimi a variável que guarda a função.
+// caso eu coloque o número 5 no prompt o console imprimiria o resultado R$500
+
+//2
+//Uma função que recebe dois parâmetros
+//dentro da função é declarada uma variável que vai guardar o valor apos o investimento
+// E usado o método switch para cada caso de investimento e que multiplica o segundo parâmentro
+// valor por um  valor fixo determinado no switch e guardado na variável valor apos investimento
+//ao final retorna a variavel apos investimento
+//fora da função é declarada duas variáveis que recebe a função, uma com os parâmentros 
+// caso seja ação o valor será 150
+// caso seja tesouro direto o valor será 200
+// no final imprimi as duas variáveis com os resultados 165 e alert "Tipo de investimento informado incorreto"
+
+//3 
+// Foi declarado 3 arrays, um com os números e outros dois para guardar os números pares e impares
+// Um loop que corre o array de números, caso os números sejam pares enviar para o array1
+// caso os números sejam impares enviar para array2
+// ao final 3 consoles um para imprimir a quantidade de números do array números 
+// e os outros dois para imprimir quantos números pares há no array1
+// e quantos números impares há no array2
+
+//4
+//Um array populado com números positivos e negativos e duas variáveis foi criadas para armazenar 
+//o maior e o menor valor do array de números
+//um loop para correr o array e caso seja o menor número guarda na variável
+// caso seja o maior número guarda na outra variável, por fim da console nas duas variáveis 
+// para ver o menor e maior número do array.
