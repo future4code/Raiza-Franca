14f3267fcf29f6aeaa4c42372aaaa78d9988202c
Raiza Leal Franca <raizale@hotmail.com>
2021-01-04 14:46:07 -0300

    Realização do Labenuforms

---
+++ labenuforms/src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import './App.css';
+import FristStep from "./components/FristStep";
+import LastStep from "./components/LastStep";
+import SecondStep from "./components/SecondStep";
+import ThirdStep from "./components/ThirdStep";
+
+export default class App extends React.Component {
+
+  state = {
+    step: 1
+  };
+  goToNextStep = () => {
+    this.setState({ step: this.state.step + 1 })
+  };
+
+  render() {
+
+
+    const renderizaEtapa = () => {
+      switch (this.state.step) {
+        case 1:
+          return <FristStep />;
+        case 2:
+          return <SecondStep />;
+        case 3:
+          return <ThirdStep />;
+        case 4:
+          return <LastStep />;
+      }
+    };
+
+
+    return (
+
+
+      <div className="App" >
+
+        { renderizaEtapa()}
+        < button onClick={this.goToNextStep}> Próxima Etapa</button >
+
+      </div >
+    );
+  }
+}
+
+
+
---
+++ labenuforms/src/components/LastStep.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+export default class LastStep extends React.Component {
+
+    render() {
+
+
+
+        return (
+            <div>
+                <h3>ETAPA FINAL: O formulário acabou</h3>
+                <p>1. Muito Obrigado por participar! Entraremos em contato¹</p>
+                
+
+
+
+            </div>
+        );
+    }
+}
+
+
